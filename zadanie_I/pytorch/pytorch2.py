8cf6e2dbe85d72f9d54b045fba5720435fc56f4b
kripso <kripsoworld@gmail.com>
2022-03-15 09:59:00 +0100

    To GPU

--- zadanie_I/pytorch/pytorch2.py
+++ zadanie_I/pytorch/pytorch2.py
@@ -30,6 +30,9 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor, Lambda
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
+
 training_data = datasets.FashionMNIST(
     root="data",
     train=True,
@@ -44,8 +47,9 @@ test_data = datasets.FashionMNIST(
     transform=ToTensor()
 )
 
-train_dataloader = DataLoader(training_data, batch_size=64)
+train_dataloader = DataLoader(training_data, num_workers=3, pin_memory=True, batch_size=64)
 test_dataloader = DataLoader(test_data, batch_size=64)
+
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
@@ -64,8 +68,9 @@ class NeuralNetwork(nn.Module):
         logits = self.linear_relu_stack(x)
         return logits
 
-model = NeuralNetwork()
 
+model = NeuralNetwork().to(device, non_blocking=True)
+# print(model)
 
 ##############################################
 # Hyperparameters
@@ -84,7 +89,6 @@ model = NeuralNetwork()
 learning_rate = 1e-3
 batch_size = 64
 epochs = 5
-
 
 
 #####################################
@@ -116,8 +120,7 @@ epochs = 5
 # We pass our model's output logits to ``nn.CrossEntropyLoss``, which will normalize the logits and compute the prediction error.
 
 # Initialize the loss function
-loss_fn = nn.CrossEntropyLoss()
-
+loss_fn = nn.CrossEntropyLoss().to(device, non_blocking=True)
 
 
 #####################################
@@ -151,6 +154,8 @@ def train_loop(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
     for batch, (X, y) in enumerate(dataloader):
         # Compute prediction and loss
+        X = X.to(device, non_blocking=True)
+        y = y.to(device, non_blocking=True)
         pred = model(X)
         loss = loss_fn(pred, y)
 
@@ -171,6 +176,8 @@ def test_loop(dataloader, model, loss_fn):
 
     with torch.no_grad():
         for X, y in dataloader:
+            X = X.to(device, non_blocking=True)
+            y = y.to(device, non_blocking=True)
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
@@ -184,16 +191,16 @@ def test_loop(dataloader, model, loss_fn):
 # We initialize the loss function and optimizer, and pass it to ``train_loop`` and ``test_loop``.
 # Feel free to increase the number of epochs to track the model's improving performance.
 
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+if __name__ == '__main__':
+    loss_fn = nn.CrossEntropyLoss().to(device, non_blocking=True)
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-epochs = 10
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train_loop(train_dataloader, model, loss_fn, optimizer)
-    test_loop(test_dataloader, model, loss_fn)
-print("Done!")
-
+    epochs = 100
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train_loop(train_dataloader, model, loss_fn, optimizer)
+        test_loop(test_dataloader, model, loss_fn)
+    print("Done!")
 
 
 #################################################################
