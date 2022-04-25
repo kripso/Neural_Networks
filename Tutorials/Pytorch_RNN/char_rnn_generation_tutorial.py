a0002f8fe8709fc68b0e2d5a4374bdfece7ff0f1
roland_rajcsanyi <kripsoworld@gmail.com>
2022-04-26 01:46:08 +0200

    pytorch RNN tutorials

--- Tutorials/Pytorch_RNN/char_rnn_generation_tutorial.py
+++ Tutorials/Pytorch_RNN/char_rnn_generation_tutorial.py
@@ -103,7 +103,7 @@ def readLines(filename):
 # Build the category_lines dictionary, a list of lines per category
 category_lines = {}
 all_categories = []
-for filename in findFiles('data/names/*.txt'):
+for filename in findFiles('./data/names/*.txt'):
     category = os.path.splitext(os.path.basename(filename))[0]
     all_categories.append(category)
     lines = readLines(filename)
